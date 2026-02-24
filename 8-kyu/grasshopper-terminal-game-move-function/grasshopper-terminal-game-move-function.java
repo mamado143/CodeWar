b26b781e3be9d1c70227d346782f16058c30fe8d
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-02-24 14:43:10 +0300

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-terminal-game-move-function/grasshopper-terminal-game-move-function.java
@@ -0,0 +1,9 @@
+public class Move {
+    public static int move(int position, int roll) {
+      // Double the roll and add it to the current position
+        position +=  + (roll * 2);
+      return position;
+        
+        
+    }
+}
