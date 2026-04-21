35b04025edb4aa8a179b584307ef9c82836c1030
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-04-21 10:10:16 +0300

    Add solution - CodeHub

---
+++ 8-kyu/volume-of-a-cuboid/volume-of-a-cuboid.java
@@ -0,0 +1,9 @@
+public class Kata {
+​
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    // Your code here
+    double result = length * width * height;
+    return result;
+  }
+  
+}
