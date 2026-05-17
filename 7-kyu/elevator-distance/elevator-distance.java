e7d7e5a1bab12e58ab87c833b6c2d4cb87669ee0
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-05-17 09:45:47 +0300

    Update solution - CodeHub

--- 7-kyu/elevator-distance/elevator-distance.java
+++ 7-kyu/elevator-distance/elevator-distance.java
@@ -1,13 +1,15 @@
 public class Kata{
   public static int elevatorDistance(int[] arr){
-    int total = 0;
+    int result = 0;
     
     for(int i = 0; i < arr.length - 1; i++){
-      int current = arr[i];
-      int next = arr[i + 1];
-      
-      total += Math.abs(current - next);
+      if(arr[i] < arr[i + 1]){
+        result += arr[i + 1] - arr[i];
+      }
+      else{
+        result += arr[i] - arr[i + 1];
+      }
     }
-    return total;
+    return result;
   }
 }
